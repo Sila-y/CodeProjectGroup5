92adc5010fdfc555638ab2f97413d444466db9a0
Shakhrom Berdiev <shakhromberdiev@gmail.com>
2025-10-23 21:14:53 +0200

    Shakhrom commit

---
+++ src/Shakhrom/oddOrEven.java
@@ -0,0 +1,12 @@
+package Shakhrom;
+
+public class oddOrEven {
+    // first week
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this is Shakhrom's solution");
+    }
+
+    // Hi
+
+}
