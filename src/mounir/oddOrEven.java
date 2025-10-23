563172fe4c8b1e61ea85a670fe5f01f7b7d8711d
mounir drigh <drigh.mounir@gmail.com>
2025-10-23 14:36:11 -0400

    mounir's solution added

---
+++ src/mounir/oddOrEven.java
@@ -0,0 +1,10 @@
+package mounir;
+
+public class oddOrEven {
+
+    // first week
+    public static void main(String[] args) {
+        System.out.println("Hello! this is mounir's solution");
+
+    }
+}
