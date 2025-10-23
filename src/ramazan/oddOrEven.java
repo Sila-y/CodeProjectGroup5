f219ec2de6b67a0fff70df035710e729f10dfaa1
ramazanaltundas <ramazan.altundas@hotmail.com>
2025-10-23 13:35:38 -0500

    ramazan's solution added

---
+++ src/ramazan/oddOrEven.java
@@ -0,0 +1,11 @@
+package ramazan;
+
+public class oddOrEven {
+
+    // first week
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this is Ramazan's solution");
+    }
+
+}
