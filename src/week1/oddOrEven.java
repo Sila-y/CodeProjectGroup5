c99df2bf18f0cd72bee9d0bbb01369c3482af6fa
Sila-y <silayorulmaz35@gmail.com>
2025-10-24 13:26:07 -0400

    Week1 question has been created under week1

--- src/week1/oddOrEven.java
+++ src/week1/oddOrEven.java
@@ -8,4 +8,14 @@ public class oddOrEven {
         System.out.println("Hello, this is Senaid's solution");
     }
 
+
+     /*
+    QUESTION 1 -Numbers --Odd & Even
+    Write a method which can identify if a given number odd or even
+
+    Ex:
+    identify(5) --> "Odd"
+    identify(6) --> "Even
+     */
+
 }
