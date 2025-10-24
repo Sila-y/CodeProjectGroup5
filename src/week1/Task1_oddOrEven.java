d0956522c70ae1bc16d66b8dd4c8af70c047b426
Sila-y <silayorulmaz35@gmail.com>
2025-10-24 13:43:07 -0400

    : is edited

--- src/week1/Task1_oddOrEven.java
+++ src/week1/Task1_oddOrEven.java
@@ -3,7 +3,7 @@ package week1;
 public class Task1_oddOrEven {
 
      /*
-    QUESTION 1 -Numbers --Odd & Even
+    QUESTION 1 -Numbers --Odd & Even:
     Write a method which can identify if a given number odd or even
 
     Ex:
