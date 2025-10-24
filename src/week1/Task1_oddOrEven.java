bbca2f89229823f86e40eec5ae61c2c48539eb11
Sila-y <silayorulmaz35@gmail.com>
2025-10-24 13:31:52 -0400

    Task1_oddOrEven question has been created under week1

---
+++ src/week1/Task1_oddOrEven.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Task1_oddOrEven {
+
+     /*
+    QUESTION 1 -Numbers --Odd & Even
+    Write a method which can identify if a given number odd or even
+
+    Ex:
+    identify(5) --> "Odd"
+    identify(6) --> "Even
+     */
+
+}
