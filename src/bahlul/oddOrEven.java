667670d38a9d1a4bb9e44fbead46446653f6d1c1
bako369 <bahlulyusifli@gmail.com>
2025-10-30 09:09:29 -0500

    Bako's commitment task1 - odd and even

--- src/bahlul/oddOrEven.java
+++ src/bahlul/oddOrEven.java
@@ -5,7 +5,23 @@ public class oddOrEven {
     // first week
     public static void main(String[] args) {
 
-        System.out.println("Hello, this is Bako's solution");
+
+        int number = 10;
+
+        System.out.println(oddOrEven(number));
+        System.out.println(oddOrEven(1));
+        System.out.println(oddOrEven(0));
+
+
     }
 
+
+    public static String oddOrEven(int num){
+
+        if(num%2!=0){
+            return "Odd";
+        }
+
+        return "Even";
+    }
 }
