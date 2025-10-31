cb974b0f200f3ef46b9b619cf205aa0b5ee9970b
Sila-y <silayorulmaz35@gmail.com>
2025-10-31 15:59:21 -0400

    Task2_FINRA question is edited

--- src/week1/Task2_FINRA.java
+++ src/week1/Task2_FINRA.java
@@ -3,15 +3,15 @@ package week1;
 public class Task2_FINRA {
 
     /*
-    Write a Java method that follows these rules:
+   Write a Java method that follows these rules:
 
-Print all numbers from 1 to 30.
+  Print all numbers from 1 to 30.
 
-For numbers that are multiples of 3, print "FIN" instead of the number.
+  For numbers that are multiples of 3, print "FIN" instead of the number.
 
-For numbers that are multiples of 5, print "RA" instead of the number.
+  For numbers that are multiples of 5, print "RA" instead of the number.
 
-For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number.
+  For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number.
      */
 
 }
