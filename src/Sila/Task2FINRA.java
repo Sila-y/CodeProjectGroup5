961dcd3c1a68f1924a8039cc5fed7fae261907fd
Sila-y <silayorulmaz35@gmail.com>
2025-11-06 13:47:24 -0500

    task done Task2FINRA

--- src/Sila/Task2FINRA.java
+++ src/Sila/Task2FINRA.java
@@ -14,4 +14,23 @@ public class Task2FINRA {
   For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number.
      */
 
+
+    public static void printFinra(){
+        for ( int i = 1; i <=30; i ++){
+            if (i % 3 == 0 && i % 5 ==0 ){
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if ( i % 5 == 0) {
+                System.out.println("RA");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFinra();
+    }
+
 }
