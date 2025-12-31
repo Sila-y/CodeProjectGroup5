d9e78b41ae13fdef7ed9d85f5742cbeacddb59be
Sila-y <silayorulmaz35@gmail.com>
2025-12-31 12:06:58 -0500

    The Task6FindTheUnique question has been created

---
+++ src/weeklyTasks/Task6FindTheUnique.java
@@ -0,0 +1,9 @@
+package weeklyTasks;
+
+public class Task6FindTheUnique {
+    /*
+    String -- Find the unique
+    Write a return method that can find thw unique characters from the String
+    Ex: unique("AAABBBCCCDEF") ===> "DEF";
+     */
+}
