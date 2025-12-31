77725ffe8775ce4462b516fa4f98b36c46084de6
Shakhrom Berdiev <shakhromberdiev@gmail.com>
2025-12-31 19:40:13 +0100

    Task6FindTheUnique update3 added

---
+++ src/Shakhrom/Task6.java
@@ -0,0 +1,34 @@
+package Shakhrom;
+import java.util.*;
+
+public class Task6 {
+
+
+    public static void main(String[] args) {
+        System.out.println(unique("AAABBBCCCDEF")); // DEF
+    }
+
+    public static String unique(String str) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.append(entry.getKey());
+            }
+        }
+
+        return result.toString();
+    }
+
+}
+
+  /*
+    String -- Find the unique
+    Write a return method that can find thw unique characters from the String
+    Ex: unique("AAABBBCCCDEF") ===> "DEF";
+     */
